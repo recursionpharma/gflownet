masking + mp messaging fix + log hps

src/gflownet/models/graph_transformer.py
@@ -137,17 +137,25 @@ class GraphTransformerGFN(nn.Module):
 
     def forward(self, g: gd.Batch, cond: torch.Tensor):
         node_embeddings, graph_embeddings = self.transf(g, cond)
+        # "Non-edges" are edges not currently in the graph that we could add
         ne_row, ne_col = g.non_edge_index
+        non_edge_embeddings = node_embeddings[ne_row] + node_embeddings[ne_col]
         # On `::2`, edges are duplicated to make graphs undirected, only take the even ones
         e_row, e_col = g.edge_index[:, ::2]
+        edge_embeddings = node_embeddings[e_row] + node_embeddings[e_col]
+
+        def _mask(x, m):
+            # mask logit vector x with binary mask m, -1000 is a tiny log-value
+            return x * m + -1000 * (1 - m)
+
         cat = GraphActionCategorical(
             g,
             logits=[
                 self.emb2stop(graph_embeddings),
-                self.emb2add_node(node_embeddings),
-                self.emb2set_node_attr(node_embeddings),
-                self.emb2add_edge(node_embeddings[ne_row] + node_embeddings[ne_col]),
-                self.emb2set_edge_attr(node_embeddings[e_row] + node_embeddings[e_col]),
+                _mask(self.emb2add_node(node_embeddings), g.add_node_mask),
+                _mask(self.emb2set_node_attr(node_embeddings), g.set_node_attr_mask),
+                _mask(self.emb2add_edge(non_edge_embeddings), g.add_edge_mask),
+                _mask(self.emb2set_edge_attr(edge_embeddings), g.set_edge_attr_mask),
             ],
             keys=[None, 'x', 'x', 'non_edge_index', 'edge_index'],
             types=self.action_type_order,

src/gflownet/utils/multiprocessing_proxy.py
@@ -22,14 +22,14 @@ class MPModelPlaceholder:
         self._is_init = True
 
     # TODO: make a generic method for this based on __getattr__
-    def logZ(self, *a):
+    def logZ(self, *a, **kw):
         self._check_init()
-        self.in_queue.put(('logZ', *a))
+        self.in_queue.put(('logZ', a, kw))
         return self.out_queue.get()
 
-    def __call__(self, *a):
+    def __call__(self, *a, **kw):
         self._check_init()
-        self.in_queue.put(('__call__', *a))
+        self.in_queue.put(('__call__', a, kw))
         return self.out_queue.get()
 
 
@@ -72,6 +72,9 @@ class MPModelProxy:
     def __del__(self):
         self.stop.set()
 
+    def to_msg(self, i):
+        return i.detach().to(torch.device('cpu')) if isinstance(i, self.cuda_types) else i
+
     def run(self):
         while not self.stop.is_set():
             for qi, q in enumerate(self.in_queues):
@@ -81,15 +84,19 @@ class MPModelProxy:
                     continue
                 except ConnectionError:
                     break
-                attr, *args = r
+                attr, args, kwargs = r
                 f = getattr(self.model, attr)
                 args = [i.to(self.device) if isinstance(i, self.cuda_types) else i for i in args]
-                result = f(*args)
+                kwargs = {k: i.to(self.device) if isinstance(i, self.cuda_types) else i for k, i in kwargs.items()}
+                result = f(*args, **kwargs)
                 if isinstance(result, (list, tuple)):
-                    msg = [i.detach().to(torch.device('cpu')) if isinstance(i, self.cuda_types) else i for i in result]
+                    msg = [self.to_msg(i) for i in result]
+                    self.out_queues[qi].put(msg)
+                elif isinstance(result, dict):
+                    msg = {k: self.to_msg(i) for k, i in result.items()}
                     self.out_queues[qi].put(msg)
                 else:
-                    msg = result.detach().to(torch.device('cpu')) if isinstance(result, self.cuda_types) else result
+                    msg = self.to_msg(result)
                     self.out_queues[qi].put(msg)
 
 