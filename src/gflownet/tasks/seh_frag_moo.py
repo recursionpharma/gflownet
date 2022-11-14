c514884de0fce36986a7386cb48a5740fbdbf675
Emmanuel Bengio <bengioe@gmail.com>
2022-11-14 14:19:10 -0500

    more flags on multiobjective hooks

--- src/gflownet/tasks/seh_frag_moo.py
+++ src/gflownet/tasks/seh_frag_moo.py
@@ -71,6 +71,9 @@ class SEHMOOTask(GFNTask):
         elif self.temperature_sample_dist == 'uniform':
             beta = self.rng.uniform(*self.temperature_dist_params, n).astype(np.float32)
             upper_bound = self.temperature_dist_params[1]
+        elif self.temperature_sample_dist == 'loguniform':
+            beta = np.exp(self.rng.uniform(*np.log(self.temperature_dist_params), n).astype(np.float32))
+            upper_bound = self.temperature_dist_params[1]
         elif self.temperature_sample_dist == 'beta':
             beta = self.rng.beta(*self.temperature_dist_params, n).astype(np.float32)
             upper_bound = 1
