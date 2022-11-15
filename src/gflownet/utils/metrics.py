87ccbaf63e2566480b759d301f0418415a130f7e
Emmanuel Bengio <emmanuel.bengio@recursionpharma.com>
2022-11-15 11:17:11 -0500

    Improve validation (#31)
    Improves metrics for multiobjective optimization: introduces Top-k and Tanimoto similarity reporting, allows tasks to define a validation set of fixed preferences that condition the data being generated for validation, logs the Pareto front explicitly, including smiles of current front.
    
    * multiobjective hooks
    
    * add hooks into training and evaluation loops
    
    * fix imports
    
    * remove determined PyTorchCallback dependency

--- src/gflownet/utils/metrics.py
+++ src/gflownet/utils/metrics.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from itertools import product
 import math
 
 from botorch.utils.multi_objective import infer_reference_point
@@ -10,6 +11,11 @@ import numpy as np
 from rdkit import Chem
 from rdkit import DataStructs
 import torch
+
+
+def generate_simplex(dims, n_per_dim):
+    spaces = [np.linspace(0.0, 1.0, n_per_dim) for _ in range(dims)]
+    return np.array([comb for comb in product(*spaces) if np.allclose(sum(comb), 1.0)])
 
 
 def pareto_frontier(obj_vals, maximize=True):
@@ -27,6 +33,33 @@ def pareto_frontier(obj_vals, maximize=True):
     else:
         pareto_mask = pareto.is_non_dominated(-torch.tensor(obj_vals))
     return obj_vals[pareto_mask]
+
+
+# From https://stackoverflow.com/questions/32791911/fast-calculation-of-pareto-front-in-python
+def is_pareto_efficient(costs, return_mask=True):
+    """
+    Find the pareto-efficient points
+    :param costs: An (n_points, n_costs) array
+    :param return_mask: True to return a mask
+    :return: An array of indices of pareto-efficient points.
+        If return_mask is True, this will be an (n_points, ) boolean array
+        Otherwise it will be a (n_efficient_points, ) integer array of indices.
+    """
+    is_efficient = np.arange(costs.shape[0])
+    n_points = costs.shape[0]
+    next_point_index = 0  # Next index in the is_efficient array to search for
+    while next_point_index < len(costs):
+        nondominated_point_mask = np.any(costs < costs[next_point_index], axis=1)
+        nondominated_point_mask[next_point_index] = True
+        is_efficient = is_efficient[nondominated_point_mask]  # Remove dominated points
+        costs = costs[nondominated_point_mask]
+        next_point_index = np.sum(nondominated_point_mask[:next_point_index]) + 1
+    if return_mask:
+        is_efficient_mask = np.zeros(n_points, dtype=bool)
+        is_efficient_mask[is_efficient] = True
+        return is_efficient_mask
+    else:
+        return is_efficient
 
 
 def get_hypervolume(flat_rewards: torch.Tensor, zero_ref=True) -> float:
