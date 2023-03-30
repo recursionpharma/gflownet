21cf05eb2dfc7ae1587b9094020eae8d112e9adc
Julien Roy <julien.roy@recursionpharma.com>
2023-03-30 13:14:20 -0600

    feat: made the replay buffer more general and added stacking of numpy and torch arrays to make multiprocessing communication (through shared memory files) more efficient

--- src/gflownet/data/replay_buffer.py
+++ src/gflownet/data/replay_buffer.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import numpy as np
+import torch
 
 
 class ReplayBuffer(object):
@@ -13,16 +14,27 @@ class ReplayBuffer(object):
         self.position = 0
         self.rng = rng
 
-    def push(self, traj, flat_rewards, cond_info):
+    def push(self, *args):
+        if len(self.buffer) == 0:
+            self._input_size = len(args)
+        else:
+            assert self._input_size == len(args), "ReplayBuffer input size must be constant"
         if len(self.buffer) < self.capacity:
             self.buffer.append(None)
-        self.buffer[self.position] = (traj, flat_rewards, cond_info)
+        self.buffer[self.position] = args
         self.position = (self.position + 1) % self.capacity
 
     def sample(self, batch_size):
         idxs = self.rng.choice(len(self.buffer), batch_size)
-        trajs, flat_rewards, cond_info = zip(*[self.buffer[idx] for idx in idxs])
-        return trajs, flat_rewards, cond_info
+        out = list(zip(*[self.buffer[idx] for idx in idxs]))
+        for i in range(len(out)):
+            # stack if all elements are numpy arrays or torch tensors
+            # (this is much more efficient to send arrays through multiprocessing queues)
+            if all([isinstance(x, np.ndarray) for x in out[i]]):
+                out[i] = np.stack(out[i], axis=0)
+            elif all([isinstance(x, torch.Tensor) for x in out[i]]):
+                out[i] = torch.stack(out[i], dim=0)
+        return tuple(out)
 
     def __len__(self):
         return len(self.buffer)
