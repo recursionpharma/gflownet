2ade0d1167a5d506a6f763c0f00950514300ffb6
Julien Roy <julien.roy@recursionpharma.com>
2023-05-11 15:08:27 -0600

    feat: added HyperPlaneShadow in GeometricRegion's

--- src/gflownet/utils/geometric_regions.py
+++ src/gflownet/utils/geometric_regions.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import Tuple
+from typing import Tuple, List
 
 import torch
 
@@ -20,6 +20,8 @@ class GeometricRegion(object):
         region_type = init_dict.pop("type")
         if region_type == "hypersphere":
             return Hypersphere(**init_dict)
+        elif region_type == "hyperplaneshadow":
+            return HyperPlaneShadow(**init_dict)
         else:
             raise ValueError(f"Unknown geometric region type {region_type}")
 
@@ -47,3 +49,29 @@ class Hypersphere(GeometricRegion):
             Tensor of shape (N,) containing the boolean mask.
         """
         return torch.norm(points - self.center, dim=1) < self.radius
+
+
+class HyperPlaneShadow(GeometricRegion):
+    """
+    Geometric region defined by a hyper-rectangle.
+    """
+
+    def __init__(self, center: Tuple[float], normal: Tuple[float]):
+        """
+        Args:
+            center: Tensor of shape (D,) containing the center of the hyperplane.
+            normal: Tensor of shape (D,) containing the normal of the hyperplane.
+        """
+        self.center = torch.tensor(center)
+        self.normal = torch.tensor(normal)
+
+    def get_sample_mask(self, points: torch.Tensor) -> torch.Tensor:
+        """
+        Returns a boolean mask indicating whether a point is beyond the hyperplane
+        i.e. on the side of the normal.
+        Args:
+            points: Tensor of shape (N, D) containing the points to check.
+        Returns:
+            Tensor of shape (N,) containing the boolean mask.
+        """
+        return torch.sum((points - self.center) * self.normal, dim=1) > 0
