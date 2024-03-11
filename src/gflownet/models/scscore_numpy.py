b3cee68b3a596d9f7910a98fa5a2252b83b6a947
julienroyd <jul.roy1311@gmail.com>
2024-03-11 15:25:39 +0000

    tox

--- src/gflownet/models/scscore_numpy.py
+++ src/gflownet/models/scscore_numpy.py
@@ -1,6 +1,6 @@
-'''
+"""
 This code computes the SC score for the recursionpharma/gflownet repository.
-Original paper: 
+Original paper:
 Original repository: https://github.com/connorcoley/scscore
 
 Specifically, we use the standalone numpy model from commit 37090a6.
@@ -15,19 +15,16 @@ This is a standalone, importable SCScorer model. It does not have tensorflow as 
 dependency and is a more attractive option for deployment. The calculations are
 fast enough that there is no real reason to use GPUs (via tf) instead of CPUs (via np)
 ===============
-'''
+"""
 
-import math, sys, random, os
+import gzip
+import json
+import math
+from pathlib import Path
+
 import numpy as np
-import time
 import rdkit.Chem as Chem
 import rdkit.Chem.AllChem as AllChem
-import json
-import gzip
-import six
-
-import os
-from pathlib import Path
 
 score_scale = 5.0
 min_separation = 0.25
@@ -35,35 +32,44 @@ min_separation = 0.25
 FP_len = 1024
 FP_rad = 2
 
-def sigmoid(x):
-  return 1 / (1 + math.exp(-x))
 
-class SCScorer():
+def sigmoid(x):
+    return 1 / (1 + math.exp(-x))
+
+
+class SCScorer:
     def __init__(self, score_scale=score_scale):
         self.vars = []
         self.score_scale = score_scale
         self._restored = False
 
     def restore(self, weight_path, FP_rad=FP_rad, FP_len=FP_len):
-        self.FP_len = FP_len; self.FP_rad = FP_rad
+        self.FP_len = FP_len
+        self.FP_rad = FP_rad
         self._load_vars(weight_path)
-        print('Restored variables from {}'.format(weight_path))
+        print("Restored variables from {}".format(weight_path))
 
-        if 'uint8' in weight_path or 'counts' in weight_path:
+        if "uint8" in weight_path or "counts" in weight_path:
+
             def mol_to_fp(self, mol):
                 if mol is None:
                     return np.array((self.FP_len,), dtype=np.uint8)
-                fp = AllChem.GetMorganFingerprint(mol, self.FP_rad, useChirality=True) # uitnsparsevect
+                fp = AllChem.GetMorganFingerprint(mol, self.FP_rad, useChirality=True)  # uitnsparsevect
                 fp_folded = np.zeros((self.FP_len,), dtype=np.uint8)
-                for k, v in six.iteritems(fp.GetNonzeroElements()):
+                for k, v in fp.GetNonzeroElements().items():
                     fp_folded[k % self.FP_len] += v
                 return np.array(fp_folded)
+
         else:
+
             def mol_to_fp(self, mol):
                 if mol is None:
                     return np.zeros((self.FP_len,), dtype=np.float32)
-                return np.array(AllChem.GetMorganFingerprintAsBitVect(mol, self.FP_rad, nBits=self.FP_len,
-                    useChirality=True), dtype=np.bool_)
+                return np.array(
+                    AllChem.GetMorganFingerprintAsBitVect(mol, self.FP_rad, nBits=self.FP_len, useChirality=True),
+                    dtype=np.bool_,
+                )
+
         self.mol_to_fp = mol_to_fp
 
         self._restored = True
@@ -76,54 +82,57 @@ class SCScorer():
 
     def apply(self, x):
         if not self._restored:
-            raise ValueError('Must restore model weights!')
+            raise ValueError("Must restore model weights!")
         # Each pair of vars is a weight and bias term
         for i in range(0, len(self.vars), 2):
-            last_layer = (i == len(self.vars)-2)
+            last_layer = i == len(self.vars) - 2
             W = self.vars[i]
-            b = self.vars[i+1]
+            b = self.vars[i + 1]
             x = np.matmul(x, W) + b
             if not last_layer:
-                x = x * (x > 0) # ReLU
+                x = x * (x > 0)  # ReLU
         x = 1 + (score_scale - 1) * sigmoid(x)
         return x
 
-    def get_score_from_smi(self, smi='', v=False):
+    def get_score_from_smi(self, smi="", v=False):
         if not smi:
-            return ('', 0.)
+            return ("", 0.0)
         fp = np.array((self.smi_to_fp(smi)), dtype=np.float32)
         if sum(fp) == 0:
-            if v: print('Could not get fingerprint?')
-            cur_score = 0.
+            if v:
+                print("Could not get fingerprint?")
+            cur_score = 0.0
         else:
             # Run
             cur_score = self.apply(fp)
-            if v: print('Score: {}'.format(cur_score))
+            if v:
+                print("Score: {}".format(cur_score))
         mol = Chem.MolFromSmiles(smi)
         if mol:
             smi = Chem.MolToSmiles(mol, isomericSmiles=True, kekuleSmiles=True)
         else:
-            smi = ''
+            smi = ""
         return (smi, cur_score)
 
     def _load_vars(self, weight_path):
-        if weight_path.endswith('pickle'):
+        if weight_path.endswith("pickle"):
             import pickle
-            with open(weight_path, 'rb') as fid:
+
+            with open(weight_path, "rb") as fid:
                 self.vars = pickle.load(fid, encoding="bytes")
                 self.vars = [x.tolist() for x in self.vars]
-        elif weight_path.endswith('json.gz'):
-            with gzip.GzipFile(weight_path, 'r') as fin:    # 4. gzip
-                json_bytes = fin.read()                      # 3. bytes (i.e. UTF-8)
-                json_str = json_bytes.decode('utf-8')            # 2. string (i.e. JSON)
+        elif weight_path.endswith("json.gz"):
+            with gzip.GzipFile(weight_path, "r") as fin:  # 4. gzip
+                json_bytes = fin.read()  # 3. bytes (i.e. UTF-8)
+                json_str = json_bytes.decode("utf-8")  # 2. string (i.e. JSON)
                 self.vars = json.loads(json_str)
                 self.vars = [np.array(x) for x in self.vars]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     model = SCScorer()
-    model.restore(str(Path(__file__).parent / 'cache' / 'scscorer_model.ckpt-10654.as_numpy.pickle'))
-    smis = ['CCCOCCC', 'CCCNc1ccccc1']
+    model.restore(str(Path(__file__).parent / "cache" / "scscorer_model.ckpt-10654.as_numpy.pickle"))
+    smis = ["CCCOCCC", "CCCNc1ccccc1"]
     for smi in smis:
         (smi, sco) = model.get_score_from_smi(smi)
-        print('%.4f <--- %s' % (sco, smi))
+        print("%.4f <--- %s" % (sco, smi))
